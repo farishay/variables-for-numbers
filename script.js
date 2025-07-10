717a7a9d2c6ddb8f6bf490258a7f538e32a1bb25
Farwa Tariq <farwa.tariq2434@gmail.com>
2025-07-10 16:44:25 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+// js assignment 3 variables for numbers
+
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+let age = 21; 
+alert("que 1 \n"+ "My age is: " + age);
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+let visitCount = 30; 
+alert("que 2 \n" + "You have visited this site " + visitCount + " times");
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+let birthYear = 2004;
+document.writeln("que 3 <br>" + "My birth year is: " + birthYear + "<br>" + "Data type of my declared variable is number <br> <br>");   
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+let visitorName = "John Doe";
+let productTitle = "T-shirt";   
+let quantity = 5;
+document.writeln("que 4 <br>" + "<b>" +  visitorName +"</b>" + " ordered " +"<b>" + quantity + " " + productTitle + "</b>"+ "(s) on XYZ Clothing store");
